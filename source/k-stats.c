4ad00333aaa4ba1081624281eb69b06a0683ef92
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2025-01-07 08:13:22 +0100

    Deleted k-stats-intern.h and moved content just to k-stats.c. Renamed default output file to output.log.

--- source/k-stats.c
+++ source/k-stats.c
@@ -2,8 +2,29 @@
  * k-stats.c - stats management
  */
 
-#include "k-stats.h"
-#include "k-stats-intern.h"
+#include <stddef.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <ncurses.h>
+
+#include "debug.h"
+
+typedef struct stats_patt_t
+{
+  size_t letter;
+  size_t trap;
+  size_t crowd;
+  size_t done;
+  size_t block;
+  size_t none;
+} stats_patt_t;
+
+typedef struct stats_t
+{
+  stats_patt_t patt;
+  size_t test;
+} stats_t;
+
 
 pthread_mutex_t stats_lock;
 
--- source/korsord.c
+++ source/korsord.c
@@ -525,7 +525,7 @@ int main(int argc, char* argv[])
   
   if(args.ncurses)
   {
-    args.output = "output.txt";
+    args.output = "output.log";
   }
 
   if(args.output)
